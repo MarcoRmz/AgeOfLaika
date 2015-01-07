972715721a120d12f21f3295dcc3953a0d57c7bd
MarcoRmz <marcormz19@gmail.com>
2015-01-07 10:29:24 -0600

    initial commit

---
+++ AgeOfLaika/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AgeOfLaika
+//
+//  Created by Marco Ramirez on 1/7/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
